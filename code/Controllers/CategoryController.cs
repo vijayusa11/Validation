d4199a574e64a5f7764a66eb4874e9f7e08bcbde
vijayusa11 <63645737+vijayusa11@users.noreply.github.com>
2022-11-16 01:32:31 +0530

    first commit

---
+++ code/Controllers/CategoryController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace code.Controllers
+{
+    public class CategoryController : Controller
+    {
+        WebAppEntities db = new WebAppEntities();
+        // GET: Category
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var category = db.tbl_category.ToList();
+            return View(category);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(tbl_category category)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.tbl_category.Add(category);
+                    db.SaveChanges();
+
+                    TempData["msg"] = "Category Added Successfully";
+                    return RedirectToAction("Create");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            catch(Exception ex)
+            {
+                TempData["msg"] = "Category Added Successfully" + ex.Message;
+                return RedirectToAction("Create");
+            }
+           
+        }
+    }
+}
---
+++ code/tbl_categoryValidation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace code
+{
+    public class tbl_categoryValidation
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Description is urgent required")]
+        public string Description { get; set; }
+    }
+
+    [MetadataType(typeof(tbl_categoryValidation))]
+    public partial class tbl_category
+    {
+
+    }
+}
